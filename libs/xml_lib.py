1365d9e47732d78e12a39c2e86b0b06cca597c1b
Antonin Carette <antonin.carette@gmail.com>
2015-06-18 16:55:02 +0200

    libs/xml_lib.py : Function to parse the smf.run.xml added

--- libs/xml_lib.py
+++ libs/xml_lib.py
@@ -53,7 +53,81 @@ def is_a_valid_XML_document(xml_parsing, xml_document):
 
     return True
 
-def decompose_mutations(xml_document, debug_mode = False):
+def parse_smf_run(smf_run_document, debug_mode = False):
+    """
+    Abstract: Function to parse the smf_run document, which contains all usefull informations about tests.
+    Return a dictionary of tests (which contains ids of their methods and variables),\
+    a dictionary of test name (to find the id) and a dictionary of case name (to find the id).
+    Notice: Each case is a method in the class test!
+    """
+
+    tree = ET.parse(smf_run_document)
+    root = tree.getroot()
+
+    tests_table = {}
+
+    test_name_to_id = {}
+
+    case_name_to_id = {}
+
+    for test in root.findall("tests"):
+
+        #get classes
+        for classes in test.findall("classes"):
+            i = 0
+            for class_item in classes.findall("class"):
+
+                #build the id
+                test_id = "t{0}".format(i)
+                test_name = class_item.text
+
+                if debug_mode:
+                    print("{0} -> {1}... ".format(test_id, test_name), end="")
+
+                #put the test in the list of available tests
+                tests_table[test_id] = { 'name' : test_name, 'items' : [] }
+
+                #save name -> id
+                test_name_to_id[test_name] = test_id
+
+                if debug_mode:
+                    print("saved!")
+
+                i = i + 1
+
+        if debug_mode:
+            print("#"*80)
+
+        #get cases
+        for cases in test.findall("cases"):
+
+            i = 0
+
+            for case_item in cases.findall("case"):
+
+                #search for the test_id to make the case_id & build the id
+                case_id = "c{0}".format(i)
+                case_name = case_item.text
+                test_id = test_name_to_id[case_name.rsplit('.', 1)[0]]
+                case_id  = test_id + case_id
+
+                if debug_mode:
+                    print("{0} in {1}... ".format(case_id, test_id), end="")
+
+                #put the node case in the list of items of the test_id
+                tests_table[test_id]['items'].append(case_id)
+
+                #save name -> id
+                case_name_to_id[case_name] = {'name' : case_id, 'original_test' : test_id}
+
+                if debug_mode:
+                    print("saved!")
+
+                i = i + 1
+
+    return tests_table, test_name_to_id, case_name_to_id
+
+def parse_mutations(mutations_document, debug_mode = False):
     """
     Abstract: Function to decompose mutations in the xml document (given as parameter), usefull to merge with use graph
     Return a list of couples (mutant, impacted_nodes)
