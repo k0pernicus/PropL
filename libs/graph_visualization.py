95c3071b44703da710b46c0c46cf67b840ead575
Antonin Carette <antonin.carette@gmail.com>
2015-06-22 12:38:11 +0200

    libs/graph_visualization.py : Script added to visualize a graph (with Python 2.7 and matplotlib)

---
+++ libs/graph_visualization.py
@@ -0,0 +1,20 @@
+import sys
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+def draw_graph(path):
+
+    graph = nx.read_graphml(path)
+
+    nx.draw_networkx(graph)
+    
+    plt.show()
+
+if __name__ == "__main__":
+
+    path = sys.argv[1]
+
+    print path
+
+    draw_graph(path)
