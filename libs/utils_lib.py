86a1d6b82d0a138205323412f42cccf48013ff4e
Antonin Carette <antonin.carette@gmail.com>
2015-06-23 16:12:35 +0200

    libs/utils_lib.py : Update typo

--- libs/utils_lib.py
+++ libs/utils_lib.py
@@ -1,6 +1,6 @@
-def chunks(list, n):
+def chunks_list(list, n):
     """
-    Yield successive n-sized chunks from l
+    Yield successive n-sized chunks from list
     """
     for i in range(0, len(list), n):
         yield list[i:i+n]
