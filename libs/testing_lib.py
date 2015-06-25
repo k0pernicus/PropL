97371fe9539f79074b6c1359d63d37d8c51f25c5
Antonin Carette <antonin.carette@gmail.com>
2015-06-25 10:49:10 +0200

    libs/testing_lib.py : Debugging

--- libs/testing_lib.py
+++ libs/testing_lib.py
@@ -26,7 +26,8 @@ def doSomeTests(usegraph):
         if usegraph.debug_mode:
             print("node {0}".format(node))
 
-        tree[node] = []
+        if not node in tree:
+            tree[node] = []
 
         nodes_stack = []
 
@@ -39,26 +40,26 @@ def doSomeTests(usegraph):
             active_node_name = usegraph.all_nodes_id[active_node_id]
 
             if usegraph.debug_mode:
-                print("active_node_name {0} / successors {1}".format(active_node_name, usegraph.all_nodes_name[active_node_name]['sources']))
+               print("active_node_name {0} / successors {1}".format(active_node_name, usegraph.all_nodes_name[active_node_name]['sources']))
 
             for source_node_id in usegraph.all_nodes_name[active_node_name]['sources']:
 
                 source_node_name = usegraph.all_nodes_id[source_node_id]
 
                 if usegraph.debug_mode:
-                    print("\tsource_node_name {0}".format(source_node_name))
+                   print("\tsource_node_name {0}".format(source_node_name))
 
                 weight_node = usegraph.all_weights[usegraph.all_nodes_position_in_weights_matrix[source_node_id]][usegraph.all_nodes_position_in_weights_matrix[active_node_id]]
 
                 random_propagation = random.uniform(0,1)
 
                 if usegraph.debug_mode:
-                    print("random_propagation {0}".format(random_propagation))
+                   print("random_propagation {0}".format(random_propagation))
 
                 if random_propagation <= weight_node :
 
                     if usegraph.debug_mode:
-                        print("\trandom_propagation <= weight of source_node_name ({0})".format(weight_node))
+                       print("\trandom_propagation <= weight of source_node_name ({0})".format(weight_node))
 
                     if not 'nt' in source_node_id:
 
@@ -66,17 +67,20 @@ def doSomeTests(usegraph):
                             tree[node].append(source_node_id)
 
                         if usegraph.debug_mode:
-                            print("\t{0} saved!".format(source_node_name))
+                           print("\t{0} saved!".format(source_node_name))
+
+                        print(tree)
 
                     else:
 
                         if usegraph.debug_mode:
-                            print("\t{0} append...".format(source_node_name))
+                           print("\t{0} append...".format(source_node_name))
+
                         nodes_stack.append(source_node_id)
 
                 else:
 
                     if usegraph.debug_mode:
-                        print("\trandom_propagation > weight of source_node_name ({0})".format(weight_node))
+                       print("\trandom_propagation > weight of source_node_name ({0})".format(weight_node))
 
     isAlgorithmGoodBetween(usegraph.path_file, usegraph.files_for_tests, usegraph.all_cases_name, usegraph.mutants, usegraph.all_nodes_name, tree)
