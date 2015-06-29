cce5e3ddda1750ac35ee08da26059873055aedc1
Antonin Carette <antonin.carette@gmail.com>
2015-06-29 10:49:34 +0200

    libs/utils_lib.py : getSomeStats has been renamed to getSomeInfos

--- libs/utils_lib.py
+++ libs/utils_lib.py
@@ -6,3 +6,16 @@ def chunksList(list, n):
     for i in range(0, len(list), n):
         list_to_return += [list[i:i+n]]
     return list_to_return
+
+def getSomeInfos(usegraph):
+    """
+    Abstract: Method to get some stats on a UseGraph object (usegraph)
+    """
+
+    print("{0}{1}{0}".format("#"*20, usegraph.id))
+    for edge_id in usegraph.usefull_edges:
+        source = usegraph.all_edges_id[edge_id]['source']
+        target = usegraph.all_edges_id[edge_id]['target']
+        weight = usegraph.all_weights[usegraph.all_nodes_position_in_weights_matrix[source]][usegraph.all_nodes_position_in_weights_matrix[target]]
+        print("## {0} ({1} -- {2}) : {3}".format(edge_id, source, target, weight))
+    print("{0}".format("#"*(40 + len(usegraph.id))))
