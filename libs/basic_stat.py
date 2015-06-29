3c6ee5dacd996b17ccebf40f07afb92805c50289
Antonin Carette <antonin.carette@gmail.com>
2015-06-29 10:49:13 +0200

    libs/basic_stat.py : Remove some useless code

--- libs/basic_stat.py
+++ libs/basic_stat.py
@@ -1,16 +1,3 @@
-def getSomeStats(usegraph):
-    """
-    Abstract: Method to get some stats on a UseGraph object (usegraph)
-    """
-
-    print("{0}{1}{0}".format("#"*20, usegraph.id))
-    for edge_id in usegraph.usefull_edges:
-        source = usegraph.all_edges_id[edge_id]['source']
-        target = usegraph.all_edges_id[edge_id]['target']
-        weight = usegraph.all_weights[usegraph.all_nodes_position_in_weights_matrix[source]][usegraph.all_nodes_position_in_weights_matrix[target]]
-        print("## {0} ({1} -- {2}) : {3}".format(edge_id, source, target, weight))
-    print("{0}".format("#"*(40 + len(usegraph.id))))
-
 def computePrecision(true_positive, false_positive):
     """
     Abstract: Simple function to compute the precision of some examples
