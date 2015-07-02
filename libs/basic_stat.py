e07674bdd916c00c152e149ffe36790ac7180882
Antonin Carette <antonin.carette@gmail.com>
2015-07-02 17:12:22 +0200

    libs/basic_stat.py : computePrecision has been modified

--- libs/basic_stat.py
+++ libs/basic_stat.py
@@ -3,6 +3,8 @@ def computePrecision(true_positive, false_positive):
     Abstract: Simple function to compute the precision of some examples
     """
 
+    if (true_positive + false_positive) == 0:
+        return 1
     return (true_positive) / (true_positive + false_positive)
 
 def computeRecall(true_positive, false_negative):
