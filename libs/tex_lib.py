e3bd850c8eaf1bc23e3bc7502c5db583fee9e33a
Antonin Carette <antonin.carette@gmail.com>
2015-07-03 18:32:07 +0200

    libs/tex_lib.py : Simple library to transcript results in LaTeX table

---
+++ libs/tex_lib.py
@@ -0,0 +1,52 @@
+path = "results_antonin.tex"
+
+def initTexFile():
+
+    init_string = "\\documentclass[a4paper]{article}\n"
+    init_string += "\\begin{document}\n"
+
+    tex_file = open(path, "a")
+    tex_file.write(init_string)
+    tex_file.close()
+
+def writeInfoTexFile(data):
+
+    len_data = len(data)
+
+    info = "\\hline\n"
+    for i in range(0, len_data - 1):
+        info += "{0}&".format(data[i])
+    info += "{0}\n".format(data[len_data - 1])
+    info += "\\\\"
+
+    tex_file = open(path, "a")
+    tex_file.write(info)
+    tex_file.close()
+
+def beginTabular(nb_columns):
+
+    typo_columns = "{"
+    typo_columns += ("|l"*nb_columns)
+    typo_columns += "|}"
+
+    tex_file = open(path, "a")
+    tex_file.write("\\begin{tabular}{0}".format(typo_columns))
+    tex_file.close()
+
+def closeTabular():
+
+    tex_file = open(path, "a")
+    tex_file.write("\\end{tabular}\n")
+    tex_file.close()
+
+def closeTexFile():
+
+    tex_file = open(path, "a")
+    tex_file.write("\\end{document}\n")
+    tex_file.close()
+
+def cleanTexFile():
+
+    tex_file = open(path, "w")
+    tex_file.truncate()
+    tex_file.close()
