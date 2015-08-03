00582b69b34e9d8d933f9a3a8de98d7a8fce40fa
k0pernicus <antonin.carette@gmail.com>
2015-08-03 15:30:39 +0200

    run.sh : Bugs correction

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-# First parameter is the usegraph used
-# Second parameter is the results directory to save results in
+# First parameter is the tests directory
+# Second parameter is the usegraph used
+# Third parameter is the results directory to save results in
 
-bool = false
-for d in "tests/*"
+bool=false
+for d in $1/*
 do
-    if ["$bool" = true]; then
-        python3.4 propl.py $d/ 10 --usegraph $1 --rslts_dir $2 --save_tex
+    if ! $bool; then
+        python3.4 propl.py $d/ 10 --usegraph $2 --rslts_dir $3 --save_tex
     else
-        python3.4 propl.py $d/ 10 --usegraph $1 --rslts_dir $2 --clean_tex --save_tex
-        bool = true
+        python3.4 propl.py $d/ 10 --usegraph $2 --rslts_dir $3 --clean_tex --save_tex
+        bool=true
     fi
 done
