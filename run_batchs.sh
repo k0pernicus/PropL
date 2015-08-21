cfe7b8128f1e7eb73d8abd309b9c07056e18886a
k0pernicus <antonin.carette@gmail.com>
2015-08-21 10:49:35 +0200

    bash script to run several batchs for a graph

---
+++ run_batchs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+i=0
+
+while [ $i -le 30 ];
+do
+
+	if [ $i -eq 0 ] ;
+		then
+			python3.4 propl.py $1 10 --usegraph usegraph_C.graphml --algorithm update_all_edges_online_opt --save_tex --rslts_dir Rslts_cluster/batchs_usegraph_C/;
+		else
+			python3.4 propl.py $1 10 --usegraph usegraph_C.graphml --algorithm update_all_edges_online_opt --save_tex --rslts_dir Rslts_cluster/batchs_usegraph_C/ --nb_batch $i;
+	fi;
+
+	i=$(($i+3));
+
+done
