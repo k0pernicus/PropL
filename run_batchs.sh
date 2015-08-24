25e0bf3b77ed9a9f71ffca475cb1b9ec05b33039
k0pernicus <antonin.carette@gmail.com>
2015-08-25 01:13:48 +0200

    run_batchs.sh : Change the results dir

--- run_batchs.sh
+++ run_batchs.sh
@@ -7,9 +7,9 @@ do
 
 	if [ $i -eq 0 ] ;
 		then
-			python3.4 propl.py $1 10 --usegraph usegraph_C.graphml --algorithm update_all_edges_online_opt --save_tex --rslts_dir Rslts_cluster/batchs_usegraph_C/;
+			python3.4 propl.py $1 10 --usegraph usegraph_C.graphml --algorithm update_all_edges_online_opt --save_tex --rslts_dir Rslts_paper/PRF/;
 		else
-			python3.4 propl.py $1 10 --usegraph usegraph_C.graphml --algorithm update_all_edges_online_opt --save_tex --rslts_dir Rslts_cluster/batchs_usegraph_C/ --nb_batch $i;
+			python3.4 propl.py $1 10 --usegraph usegraph_C.graphml --algorithm update_all_edges_online_opt --save_tex --rslts_dir Rslts_paper/PRF/ --nb_batch $i;
 	fi;
 
 	i=$(($i+3));
