aa58268d1389300b7837641c1bd74902af9aaccb
k0pernicus <antonin.carette@gmail.com>
2015-08-25 01:13:25 +0200

    run.sh : remove nb_batch option

--- run.sh
+++ run.sh
@@ -12,9 +12,9 @@ do
   if [ "$4" != "tag_on_usefull_edges" ]; then
     echo "update_edges"
     if $bool; then
-        python3.4 propl.py $d/ 10 --usegraph $2 --nb_batch 20 --rslts_dir $3 --save_tex --algorithm $4
+        python3.4 propl.py $d/ 10 --usegraph $2 --rslts_dir $3 --save_tex --algorithm $4
     else
-        python3.4 propl.py $d/ 10 --usegraph $2 --nb_batch 20 --rslts_dir $3 --clean_tex --save_tex --algorithm $4
+        python3.4 propl.py $d/ 10 --usegraph $2 --rslts_dir $3 --clean_tex --save_tex --algorithm $4
         bool=true
     fi
   else
