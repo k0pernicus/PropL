e75a7265593877265127fccf370ab296592daa1e
k0pernicus <antonin.carette@gmail.com>
2015-08-25 22:10:04 +0200

    libs/basic_stat_lib.py : Function to compute f0.5 score added

--- libs/basic_stat_lib.py
+++ libs/basic_stat_lib.py
@@ -1,5 +1,7 @@
 #Python3.4 - Antonin Carette
 
+import math
+
 def computePrecision(true_positive, false_positive):
     """
     Abstract: Simple function to compute the precision of some examples
@@ -37,6 +39,20 @@ def computeFScore(precision, recall):
         return 0
     return 2 * ((precision * recall) / (precision + recall))
 
+def computeF05Score(precision, recall):
+    """
+    Abstract: Simple function to compute the F0.5 Score of some examples
+    precision: The precision of examples
+    recall: The recall of examples
+    """
+
+    if (precision + recall) == 0:
+        return 0
+
+    beta_square = math.pow(0.5, 2)
+
+    return ((beta_square + 1) * ((precision * recall)) / ((beta_square * precision) + recall))
+
 def computeAverage(list):
     """
     Abstract: Simple function to compute the average of elements in the list
@@ -48,7 +64,7 @@ def computeAverage(list):
     #if the list is empty, return 0
     if len_list == 0:
         return 0
-    
+
     #else, compute the average score
     sum_elements = 0
     for element in list:
